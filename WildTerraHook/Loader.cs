f0518df0bea2e5057c6b03f72aadf4ef9df822a6
BrowarPL <browar9000@gmail.com>
2026-01-20 14:39:04 +0000

    #

---
+++ WildTerraHook/Loader.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace WildTerraHook
+{
+    public class Loader
+    {
+        private static GameObject _loadObject;
+
+        // Tę metodę wywoła Twój injector
+        public static void Init()
+        {
+            // Sprawdź, czy hack już nie działa, aby nie stworzyć duplikatów
+            if (GameObject.Find("WildTerraHook_Loader") != null) return;
+
+            // Tworzymy pusty obiekt w świecie gry
+            _loadObject = new GameObject();
+            _loadObject.name = "WildTerraHook_Loader";
+
+            // Dodajemy do niego nasz główny skrypt MainHack
+            _loadObject.AddComponent<MainHack>();
+
+            // Mówimy Unity, aby nie niszczyło tego obiektu przy zmianie sceny (ładowaniu mapy)
+            UnityEngine.Object.DontDestroyOnLoad(_loadObject);
+        }
+
+        public static void Unload()
+        {
+            if (_loadObject != null)
+            {
+                UnityEngine.Object.Destroy(_loadObject);
+            }
+        }
+    }
+}
